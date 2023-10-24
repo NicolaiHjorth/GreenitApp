abfd476b4c27b47a0848f97856777120402b6960
Mikkelmandemand <mstrandbygaard12@hotmail.com>
2023-10-24 09:46:40 +0200

    Url 5079

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -22,6 +22,13 @@ builder.Services.AddScoped<IPostLogic, PostLogic>();
 
 var app = builder.Build();
 
+//Allowing Blazor to Access WEBAPI
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
